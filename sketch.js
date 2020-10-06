782949f85e00f500ccff4b8fb965e4f42b76f89f
amritanag1 <71591837+amritanag1@users.noreply.github.com>
2020-10-06 20:13:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,6 +8,17 @@ function setup() {
   movingRect = createSprite(400,200,80,30);
   movingRect.shapeColor = "green";
   movingRect.debug = true;
+  gameobject1 = createSprite(100,100,50,50);
+  gameobject1.shapeColor = "green";
+   gameobject2 = createSprite(200,100,50,50);
+  gameobject2.shapeColor = "green";
+   gameobject3 = createSprite(300,100,50,50);
+  gameobject3.shapeColor = "green";
+   gameobject4 = createSprite(400,100,50,50);
+  gameobject4.shapeColor = "green";
+  
+  
+  
 }
 
 function draw() {
@@ -26,5 +37,20 @@ function draw() {
     movingRect.shapeColor = "green";
     fixedRect.shapeColor = "green";
   }
+  bounceOff();
   drawSprites();
+}
+
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+      && object2.x - object1.x < object2.width/2 + object1.width/2){
+    object1.velocityX = object1.velocityX*(-1);
+    object2.velocityX = object2.velocityX*(-1);
+  }
+  if (object1.y - object2.y < object2.height/2 + object1.height/2
+      && object2.y - object1.y < object2.height/2 + object1.height/2){
+    object1.velocityY = object1.velocityY*(-1);
+    object2.velocityY = object2.velocityY*(-1);
+  }
+  
 }
